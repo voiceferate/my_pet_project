26f1f2c4aee22b21f9d726f2b6d7296613775543
voiceferate <vanyavi06@gmail.com>
2017-07-06 16:08:22 +0300

    added js label circles

--- app/js/_my_features.js
+++ app/js/_my_features.js
@@ -2,9 +2,30 @@
 
 (function() {
 
-var my_element = document.querySelectorAll('p.content-card__descr');
-    
-console.log(my_element[2].offsetTop);
-console.log(my_element[3].offsetTop);
-console.log(my_element[5].offsetTop);
+
+function createLeftCircle() {
+  var circle_displayed_left = document.createElement('div');
+  circle_displayed_left.className = 'content-card_left-label';
+  return circle_displayed_left
+}
+
+function createRightCircle() {
+  var circle_displayed_right = document.createElement('div');
+  circle_displayed_right.className = 'content-card_right-label';
+  return circle_displayed_right;
+}
+
+
+
+
+var circle_parent_element = document.querySelectorAll('div.content-card');
+
+for (var i = circle_parent_element.length - 1; i >= 0; i--) {
+  if ( circle_parent_element[i].className == 'content-card content-card_left' ) {
+    circle_parent_element[i].appendChild(createLeftCircle());
+  } else {
+    circle_parent_element[i].appendChild(createRightCircle());
+  }
+}
+
 })();
